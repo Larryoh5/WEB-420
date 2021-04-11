35559ab954f479a6dd27775ba5dbeab7391111a0
Larry Ohaka <larryohaka@ymail.com>
2021-04-11 11:09:26 -0400

    new

---
+++ api-gateway/models/user.js
@@ -0,0 +1,27 @@
+/*
+============================================
+; Title:  User Model
+; Author: Professor Krasso
+; Date:   11 April 2021
+; Modified by: Larry Ohaka
+; Description: This is our user model.
+;===========================================
+*/
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  email: String,
+});
+
+const User = (module.exports = mongoose.model("User", userSchema));
+
+// add new user to db
+module.exports.add = (user, callback) => {
+  user.save(callback);
+};
+
+module.exports.getById = (id, callback) => {
+  var query = { _id: id };
+  User.findById(query, callback);
+};
